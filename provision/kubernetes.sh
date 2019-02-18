9bc04424e28b4de6a9698387f473771e14f2263f
linyows <linyows@gmail.com>
2019-02-18 18:27:00 +0900

    specify weave network

--- provision/kubernetes.sh
+++ provision/kubernetes.sh
@@ -46,6 +46,10 @@ IP=$(ifconfig enp0s8 | grep inet | awk '{print $2}' | cut -d':' -f2)
 echo "KUBELET_EXTRA_ARGS=\"--node-ip=$IP --cluster-dns=$CLUSTERIP\"" > /etc/default/kubelet
 systemctl restart kubelet
 
+# Add routing to LB by enp0s3
+route add 10.0.2.15 gw 172.16.20.11
+route add 10.32.0.1 gw 172.16.20.11
+
 echo $HOSTNAME | grep -q 'master'
 # Master Node
 if [ $? -eq 0 ]; then
@@ -60,6 +64,7 @@ if [ $? -eq 0 ]; then
     # Export to shared dir
     rm -rf /vagrant/shared/kubernetes
     cp -R /etc/kubernetes /vagrant/shared
+    sed -i 's/10\.0\.2\.15/172.16.20.10/g' /vagrant/shared/kubernetes/manifests/kube-apiserver.yaml
   else
     # Import from shared dir
     mkdir -p /etc/kubernetes/pki/etcd
